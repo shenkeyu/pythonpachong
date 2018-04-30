65188b233714f8aebc388cc39e25b031a45436cd
shenkeyu <fengyangluoyue@163.com>
2018-04-30 13:27:54 +0800

    add sql database sqlite3 and nosql database mongodb and redis new

--- DataOutput.py
+++ DataOutput.py
@@ -1,6 +1,10 @@
 #coding:utf-8
 import codecs
 import time
+import sqlite3
+import pymongo
+import datetime
+import redis
 from pyecharts import Bar
 
 class DataOutput(object):
@@ -41,3 +45,62 @@ class DataOutput(object):
     def out_putchart(self):
         bar=Bar("船只进港情况")
         #bar.add("船只数量","shhd",23,render())
+
+    def store_sqlite3(self,td):
+        con=sqlite3.connect('chuanbo.db')
+        cur=con.cursor()
+        #cur.execute('CREATE TABLE chuanbo (id integer primary key,gangquname varchar(20),chuanname varchar(20),huhao varchar(10),MMSI integer,zhuangtai varchar(10),shijian varchar(25))')
+        i=0
+        for ts in td:
+            gangqunname=td[i]
+            chuanname=td[i+1]
+            huhao=td[i+2]
+            MMSI=td[i+3]
+            zhuangtai=td[i+4]
+            shijian=td[i+5]
+            i=i+6
+            cur.execute('INSERT INTO chuanbo VALUES (?,?,?,?,?,?)',gangqunname,chuanname,huhao,MMSI,zhuangtai,shijian)
+        try:
+            con.commit()
+        except Exception:
+            print "提交失败"
+        con.close()
+
+
+    def store_mongodb(self,td):
+        client = pymongo.MongoClient('mongodb://localhost:27017/')
+        db = client.papers
+        collection = db.chuanbo
+        i=0
+        for ts in td:
+            chuan = {"gangqunname": td[i],
+                    "chuanname": td[i+1],
+                    "huhao": td[i+2],
+                    "MMSI":td[i+3],
+                    "zhuangtai":td[i+4],
+                    "shijian":td[i+5],
+                    "date": datetime.datetime.utcnow()
+                    }
+            chuan_id = collection.insert(chuan)
+            i = i + 6
+        collection.find_one({"gangqunname": "厦门"})
+        for chuan in collection.find():
+            print chuan
+        for chuan in collection.find({"gangqunname": "厦门"}):
+            print chuan
+        client.close()
+
+    def store_redis(self,td):
+        r = redis.Redis(host='127.0.0.1', port=6379)
+        r.set('name', 'qiye')
+        i=0
+        for ts in td:
+            r.set("gangqunname", td[i])
+            r.set("chuanname",td[i+1])
+            r.set("huhao",td[i+2])
+            r.set("MMSI",td[i+3])
+            r.set("zhuangtai",td[i+4])
+            r.set("shijian",td[i+5])
+            i = i + 6
+        print r.get('gangqunname')
+
